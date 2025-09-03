6884577c68059ec9ce22c385b3de1b55b9b96f62
Milo Niemaz <mniemaz@z3r3p6.42lyon.fr>
2025-09-03 17:00:59 +0200

    norm

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mniemaz <mniemaz@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 02:47:44 by miloniemaz        #+#    #+#             */
-/*   Updated: 2025/09/03 16:40:37 by mniemaz          ###   ########.fr       */
+/*   Updated: 2025/09/03 16:57:46 by mniemaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,15 @@ int main()
     const Animal* animal = new Animal();
     const Animal* dog = new Dog();
     const Animal* cat = new Cat();
-    std::cout << dog->getType() << " " << std::endl;
-    std::cout << cat->getType() << " " << std::endl;
+    std::cout << "--" << std::endl;
+    std::cout << "type: " << dog->getType() << " " << std::endl;
+    std::cout << "type: " << cat->getType() << " " << std::endl;
+    std::cout << "--" << std::endl;
     cat->makeSound();
     dog->makeSound();
     animal->makeSound();
     
+    std::cout << "--" << std::endl;
     delete(animal);
     delete(cat);
     delete(dog);
